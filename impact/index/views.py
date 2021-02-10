7f3978fdfa86a9ec618450dad3f4a41fedc50ca1
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-10 17:43:43 -0600

    making base view

--- impact/index/views.py
+++ impact/index/views.py
@@ -5,4 +5,7 @@ index = Blueprint('index', __name__)
 @index.route('/')
 def index():
     return render_template('index.html')
-    
+
+@index.route('/about')
+def about():
+    return render_template('about.html')
