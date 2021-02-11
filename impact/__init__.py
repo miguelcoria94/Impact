0bef6f3d4fed41376f321420f8dc658b45b11024
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-10 19:44:15 -0600

    made 404 and 403 error pages

--- impact/__init__.py
+++ impact/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from impact.core.views import core
+from impact.errors.error_handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
