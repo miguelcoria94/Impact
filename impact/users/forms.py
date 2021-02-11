bb981598122467381d9714a7ff0c2e5f860924c1
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-10 22:42:47 -0600

    done with user form

---
+++ impact/users/forms.py
@@ -0,0 +1,44 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
+
+from flask_login import current_user
+from impact.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('UserName', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired, EqualTo('confirm_password', message="Passwords must match!")])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Create Account")
+    
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("This email is already in use!")
+
+    def check_username(self, field):
+        if User.query.filer_by(username=field.data).first():
+            raise ValidationError("Username is already taken!")
+
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('UserName', validators=[DataRequired()])
+    avatar = FileField('Update your Profile Picture',
+                       validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("This email is already in use!")
+
+    def check_username(self, field):
+        if User.query.filer_by(username=field.data).first():
+            raise ValidationError("Username is already taken!")
