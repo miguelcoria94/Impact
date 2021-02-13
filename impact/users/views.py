04b69e9d42f6da01103fef5045e6b067d9fc6768
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-12 23:52:00 -0600

    hardest view

--- impact/users/views.py
+++ impact/users/views.py
@@ -78,4 +78,6 @@ def account():
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    
+    posts = Post.query.filter_by(author=user).order_by(Post.date.desc()).paginate(page=page, per_page=5)
+    return render_template(('user_posts.html'), posts=posts, user=user)
+
