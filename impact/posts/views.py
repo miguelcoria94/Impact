24e4a2c97e5a2aa9df3416b5ea6375ae88cb0658
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-18 23:56:33 -0600

    create route to create a post

--- impact/posts/views.py
+++ impact/posts/views.py
@@ -5,3 +5,11 @@ from impact.models import Post
 from impact.posts.forms import PostForm
 
 blog_posts = Blueprint('blog_post', __name__)
+
+@blog_posts.route('/create', methods=["GET", "POST"])
+@login_required
+def create_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        blog_post = Post(title=)
