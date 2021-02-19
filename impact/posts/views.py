39f492a0053c5a4256b596b55beeae1232851663
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-19 00:22:46 -0600

    done with delete route

--- impact/posts/views.py
+++ impact/posts/views.py
@@ -25,3 +25,42 @@ def blog_post(blog_post_id):
     blog_post = Post.query.get_or_404(blog_post_id)
 
     return render_template("blog_post.html", title=blog_post.title, date=blog_post.date, post=blog_post)
+
+
+@blog_post.route('/<int:blog_post_id/update', methods=['GET', 'POST'])
+@login_required
+def update(blog_post_id):
+
+    blog_post = Post.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        abort(403)
+    
+    form = PostForm()
+
+    if form.validate_on_submit():
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+
+        db.session.commit()
+        return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_post.id))
+    
+    elif request.method == 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template('create_post.html', title="Updating", form=form)
+
+
+@blog_post.route('/<int:blog_post_id/delete', methods=['GET', 'POST'])
+@login_required
+def delete_post(blog_post_id):
+
+    blog_post = Post.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+
+    db.session.delete(blog_post)
+    db.session.commit()
+
+    return render_template(url_for('core.index'))
